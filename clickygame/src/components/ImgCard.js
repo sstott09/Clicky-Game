8b9f222becf4805a4d53bfced8dfa23e72f57cd5
Steven Stot <sstott09@yahoo.com>
2020-03-10 12:20:46 -0600

    Revised Styling and Created ReadMe

--- clickygame/src/components/ImgCard.js
+++ clickygame/src/components/ImgCard.js
@@ -1,17 +1,20 @@
-import React from  "react";
-import { Container, Row, Col } from 'reactstrap';
+import React from "react";
+import {Container, Row, Col} from 'reactstrap';
 import "./style.css"
 
-function ImgCard(props){
-    return(
+function ImgCard(props) {
+    return (
         <Container>
-      <Row sm="2">
-        <Col>      
-            <img className="image-cards" src ={props.image} alt="imageCard" onClick={()=> props.handleImageClick(props.id)}></img>
-        </Col>
-      </Row>
-    </Container>
-  );
+            <Row sm="2">
+                <Col>
+                    <img className="image-cards zoom" src ={props.image} alt="imageCard"
+                        onClick={
+                            () => props.handleImageClick(props.id)
+                    }></img>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default ImgCard;
--- clickygame/src/components/Nav.js
+++ clickygame/src/components/Nav.js
@@ -1,15 +1,21 @@
-import React from  "react";
+import React from "react";
 import "./style.css"
 
-function Nav(props){
-    return(
-        <nav className="navbar">
-            <ul>
-                <h1 className="brand">Clicky Game</h1>
+function Nav(props) {
+    return (<nav className="navbar">
 
-                <li>Score: {props.score} | TopScore: {props.topScore}</li>
-            </ul>
-        </nav>
+        <ul>
+            <h1 className="brand">Puppy Dog Clicky Game</h1>
+            <h2 className="instructions">Click on a puppy image below to earn points. If you click on the same puppy twice > GAME OVER!</h2>
+
+            <li>Score: {
+                props.score
+            }
+                | TopScore: {
+                props.topScore
+            }</li>
+        </ul>
+    </nav>
     )
-}
-export default Nav;
+        }
+        export default Nav;
